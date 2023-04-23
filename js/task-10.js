9ab3f9e85c90d17797d7a1133c97bbca6d850cdc
TarasMysiura <tarasmysiura@gmail.com>
2023-04-23 08:56:38 +0300

    add

--- js/task-10.js
+++ js/task-10.js
@@ -26,39 +26,88 @@
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб
 //  видаляючи всі створені елементи.
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 const inputEl = document.querySelector('[type="number"]');
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-create]");
 const divBox = document.querySelector("#boxes");
 
-const amount = inputEl.addEventListener("change", (event) => {
-  const amount = event.currentTarget.value;
-  console.log(amount);
-});
-const clickCreate = createBtn.addEventListener("change", createBoxes(amount));
-const clickDestroy = destroyBtn.addEventListener("change", destroyBoxes);
+const amount = inputEl.addEventListener("blur", addAmount);
 
-console.log(clickCreate);
-
-
-// const amount = inputEl.addEventListener("change", createBoxes);
-
-function createBoxes(amount) {
-  for (let color = 0; color < amount; color += 1) {
-    const item = divBox.createElement("div");
-    item.textContent = getRandomHexColor();
-    divBox.append(item);
-    console.log(item);
-  }  
+function addAmount(event) {
+  const amountEl = event.currentTarget.value;
+  // createBoxes(amou)
+  return amountEl;
 }
 
-function destroyBoxes() {
-  
+console.log(amount);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+// const clickCreate = createBtn.addEventListener("change", createBoxes(amount));
+// const clickDestroy = destroyBtn.addEventListener("change", destroyBoxes);
+
+// console.log(clickCreate);
+
+// // const amount = inputEl.addEventListener("change", createBoxes);
+
+// function createBoxes(amount) {
+//   for (let color = 0; color < amount; color += 1) {
+//     const item = divBox.createElement("div");
+//     item.textContent = getRandomHexColor();
+//     divBox.append(item);
+//     console.log(item);
+//   }
+// }
+
+// function destroyBoxes() {
+
+// }
 
 // document.body.prepend(ulEl);
+
+// const colorPalette = document.querySelector(".color-palette");
+// const output = document.querySelector(".output");
+
+// colorPalette.addEventListener("click", selectColor);
+
+// This is where delegation «magic» happens
+// function selectColor(event) {
+//   if (event.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+
+//   const selectedColor = event.target.dataset.color;
+//   output.textContent = `Selected color: ${selectedColor}`;
+//   output.style.color = selectedColor;
+// }
+
+// // Some helper functions to render palette items
+// createPaletteItems();
+
+// function createPaletteItems() {
+//   const items = [];
+//   for (let i = 0; i < 60; i++) {
+//     const color = getRandomColor();
+//     const item = document.createElement("button");
+//     item.type = "button";
+//     item.dataset.color = color;
+//     item.style.backgroundColor = color;
+//     item.classList.add("item");
+//     items.push(item);
+//   }
+//   colorPalette.append(...items);
+// }
+
+// function getRandomColor() {
+//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+// }
+
+// function getRandomHex() {
+//   return Math.round(Math.random() * 256)
+//     .toString(16)
+//     .padStart(2, "0");
+// }
